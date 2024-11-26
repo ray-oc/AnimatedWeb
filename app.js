40536f1ea39ae2bac00e313fefdf3ae292121655
The octocode <163339302+ray-oc@users.noreply.github.com>
2024-11-26 20:29:09 +0100

    Commit

---
+++ app.js
@@ -0,0 +1,140 @@
+// Page one
+const tlIntro = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".first-page",
+    start: "0%",
+    end: "100%",
+    pin: true,
+    pinSpacing: false,
+  },
+});
+
+// Highlight page two
+const tlH = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    // markers: { startColor: "blue", endColor: "blue" },
+    scrub: true,
+    start: "-40%",
+    end: "40%",
+  },
+});
+
+tlH.fromTo(
+  ".highlight",
+  { color: "rgba(255,255,255,0.4)" },
+  { color: "rgba(255,255,255,1)", stagger: 1 }
+);
+
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    // markers: { startColor: "yellow", endColor: "yellow" },
+    scrub: true,
+    start: "-20%",
+    end: "60%",
+  },
+});
+
+tlHRemove.to(".highlight", { color: "rgba(255,255,255,0.4)", stagger: 1 });
+
+// Page three
+const tlSplit = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    // markers: { startColor: "purple", endColor: "purple" },
+    start: "-25%",
+    end: "30%",
+    scrub: true,
+  },
+});
+
+tlSplit.fromTo(".large-phone", { x: "40%" }, { x: "20%" });
+tlSplit.fromTo(".small-phone", { x: "-40%" }, { x: "-20%" }, "<");
+tlSplit.fromTo(
+  ".product-text-left",
+  { x: 50, opacity: 0 },
+  { x: 0, opacity: 1 },
+  "<"
+);
+tlSplit.fromTo(
+  ".product-text-right",
+  { x: 50, opacity: 0 },
+  { x: 0, opacity: 1 },
+  "<"
+);
+
+const tlSplitPin = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    pin: true,
+    pinSpacing: false,
+    start: "0%",
+    end: "100%",
+  },
+});
+
+// Carusel => Page 4
+const swatches = document.querySelectorAll(".swatches img");
+const gallery = document.querySelector(".phone-gallery");
+const slides = document.querySelectorAll(".phone-gallery-container");
+
+let currentSwatch = "blue";
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+  const coord = slides[index].getBoundingClientRect().left;
+  swatch.addEventListener("click", (e) => {
+    let swatchName = e.target.getAttribute("swatch");
+    let closeUp = document.querySelector("." + swatchName);
+    // Chekk if we are in the same swatch
+    if (currentSwatch === swatchName) return;
+
+    gsap.fromTo(closeUp, { opacity: 0 }, { opacity: 1, duration: 1 });
+    gsap.fromTo(closeUp, { zIndex: topIndex }, { zIndex: topIndex });
+
+    // Gallery
+    gsap.to(gallery, { x: -coord, duration: 1, ease: "back.out(1.2)" });
+    // Increment z-index
+    topIndex++;
+    currentSwatch = swatchName;
+  });
+});
+
+// Page five
+
+const tlVideo = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".fifth-page",
+    start: "0%",
+    end: "100%",
+    scrub: true,
+    pin: true,
+  },
+});
+
+tlVideo.fromTo(
+  ".product-video",
+  { currentTime: 0 },
+  { currentTime: 3, duration: 1 }
+);
+tlVideo.fromTo(
+  ".product-info-container h3",
+  { opacity: 0 },
+  { opacity: 1, stagger: 0.25, duration: 0.5 },
+  "<"
+);
+
+// Page six
+const tlParallax = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".sixth-page",
+    start: "-25%",
+    end: "50%",
+    scrub: true,
+  },
+});
+
+tlParallax.fromTo(".photo-description", { y: 0 }, { y: -80 });
+tlParallax.fromTo(".portrait-container", { y: 0 }, { y: -80 }, "<");
+tlParallax.fromTo(".phone-video", { y: 0 }, { y: -200 }, "<");
